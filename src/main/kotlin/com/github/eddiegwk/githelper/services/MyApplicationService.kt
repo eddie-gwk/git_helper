8d237e584c3fe6bbe4d9928eed4c3341157b5971
GitHub Action <action@github.com>
2022-11-04 06:59:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/eddiegwk/githelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/eddiegwk/githelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.eddiegwk.githelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.eddiegwk.githelper.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/eddiegwk/githelper/MyPluginTest.kt
+++ src/test/kotlin/com/github/eddiegwk/githelper/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.eddiegwk.githelper
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
